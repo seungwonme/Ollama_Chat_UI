3c214911dbd4b3042a39ee08477eea383cd2b7a3
seungwonme <seungwonan.me@gmail.com>
2024-12-14 22:15:35 +0900

    Initial commit

--- chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ChatView, chat_detail, message_list, generate
-
-urlpatterns = [
-    path("chats/", ChatView.as_view()),
-    path("chats/<int:pk>/", chat_detail),
-    path("chats/<int:chat_id>/messages/", message_list),
-    path("generate/", generate),
-]
